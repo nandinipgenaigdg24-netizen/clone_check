c9cf32684cef797b9551fdc46c61c96797833dcb
keshitrivedi <keshitrivedi@gmail.com>
2025-09-03 22:12:20 +0530

    Add setup

---
+++ components/Hero.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+
+export default function Hero () {
+    return (
+    <>
+        <div className="bg-amber-500">Yawrr mujhe nankhatai chahiyeee</div>
+    </>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import '../styles/globals.css'
+import Hero from '../components/Hero.jsx'
+import About from '../components/About.jsx'
+import Gallery from '../components/Gallery.jsx'
+import Tracks from '../components/Tracks.jsx'
+import Prizes from '../components/Prizes.jsx'
+import Schedule from '../components/Schedule.jsx'
+import Sponsors from '../components/Sponsors.jsx'
+import ContactUs from '../components/ContactUs.jsx'
+import Faq from '../components/Faq.jsx'
+
+function App() {
+  return (
+    <>
+    <Hero />
+    <About />
+    <Gallery />
+    <Tracks />
+    <Prizes />
+    <Schedule />
+    <Sponsors />
+    <ContactUs />
+    <Faq />
+    </>
+  )
+}
+
+export default App
